3ed20b6fe16b53531c679537e42ccd71d05219ab
Nicholas DiGirolamo <94650496+nsdigirolamo@users.noreply.github.com>
2022-01-27 17:47:54 -0500

    Add files via upload

---
+++ src/main/java/io/github/nsdigirolamo/powerportals/commands/Portals.java
@@ -0,0 +1,36 @@
+package io.github.nsdigirolamo.powerportals.commands;
+
+import io.github.nsdigirolamo.powerportals.structures.PowerPortal;
+import io.github.nsdigirolamo.powerportals.utils.PortalStorageUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Portals is a command that allows players to see a list of PowerPortals they own.
+ */
+public class Portals implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                int count = 0;
+                String message = ChatColor.GREEN + "[PowerPortals]\n";
+                for (PowerPortal portal: PortalStorageUtil.getPortals()) {
+                    if (portal.getOwner().getUniqueId().equals(player.getUniqueId())) {
+                        count++;
+                        message = message + count +" ... " + portal.getName() + " at (" + portal.getX() + ", " +
+                                portal.getY() + ", " + portal.getZ() + ")\n";
+                    }
+                }
+                player.sendMessage(message);
+            } else {
+                player.sendMessage(ChatColor.RED + "[PowerPortals] Invalid arguments. Use /portals");
+            }
+        }
+        return true;
+    }
+}
