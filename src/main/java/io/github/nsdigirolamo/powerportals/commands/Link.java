86c0d768885d9c47642a70848d447bcdddb6d7bb
Nicholas DiGirolamo <94650496+nsdigirolamo@users.noreply.github.com>
2022-01-27 17:22:56 -0500

    added beacon activate sound

--- src/main/java/io/github/nsdigirolamo/powerportals/commands/Link.java
+++ src/main/java/io/github/nsdigirolamo/powerportals/commands/Link.java
@@ -3,6 +3,9 @@ package io.github.nsdigirolamo.powerportals.commands;
 import io.github.nsdigirolamo.powerportals.structures.PowerPortal;
 import io.github.nsdigirolamo.powerportals.utils.PortalStorageUtil;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -34,6 +37,16 @@ public class Link implements CommandExecutor {
 
                         entrance.setEntrance(true);
                         entrance.setExit(exit);
+
+                        for (Block block: entrance.getTriggerBlocks()) {
+                            block.setType(Material.WATER, false);
+                        }
+
+                        for (Block block: exit.getTriggerBlocks()) {
+                            block.setType(Material.WATER, false);
+                        }
+
+                        player.playSound(player.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 1, 1);
 
                     } else {
                         player.sendMessage(ChatColor.RED + "[PowerPortals] Portal " + args[0] + " does not exist.");
