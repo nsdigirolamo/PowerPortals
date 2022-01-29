07e357d0f2dceb8663b3f7af7eb6021d73543b77
Nicholas DiGirolamo <94650496+nsdigirolamo@users.noreply.github.com>
2022-01-29 10:38:59 -0500

    added onEnable loads the .json storage file

--- src/main/java/io/github/nsdigirolamo/powerportals/PowerPortals.java
+++ src/main/java/io/github/nsdigirolamo/powerportals/PowerPortals.java
@@ -6,16 +6,32 @@ import io.github.nsdigirolamo.powerportals.listeners.ActivatedPortalListener;
 import io.github.nsdigirolamo.powerportals.listeners.TriggeredPortalListener;
 import io.github.nsdigirolamo.powerportals.listeners.WaterPortalListener;
 import io.github.nsdigirolamo.powerportals.utils.PortalCreationUtil;
+import io.github.nsdigirolamo.powerportals.utils.PortalStorageUtil;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PowerPortals extends JavaPlugin {
 
     private static PowerPortals plugin;
+    private static FileConfiguration config;
 
     @Override
     public void onEnable() {
+
+        config = this.getConfig();
+        config.addDefault("database.host", "localhost");
+        config.addDefault("database.port", 3306);
+        config.addDefault("database.database", "powerportals");
+        config.addDefault("database.user", "admin");
+        config.addDefault("database.password", "password");
+        config.options().copyDefaults(true);
+        saveConfig();
+
         plugin = this;
+
         PortalCreationUtil.loadDesigns();
+        PortalStorageUtil.loadPortals();
+
         this.getCommand("link").setExecutor(new Link());
         this.getCommand("portals").setExecutor(new Portals());
         getServer().getPluginManager().registerEvents(new ActivatedPortalListener(), this);
@@ -29,5 +45,9 @@ public final class PowerPortals extends JavaPlugin {
 
     public static PowerPortals getPlugin() {
         return plugin;
+    }
+
+    public static FileConfiguration config() {
+        return config;
     }
 }
