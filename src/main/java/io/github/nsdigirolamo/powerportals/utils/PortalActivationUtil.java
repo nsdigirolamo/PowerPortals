7fa7b885213cc2b37557aeab907cc747799d7ce7
Nicholas DiGirolamo <94650496+nsdigirolamo@users.noreply.github.com>
2022-01-29 13:40:34 -0500

    added check to see if portal entrance is blocked

--- src/main/java/io/github/nsdigirolamo/powerportals/utils/PortalActivationUtil.java
+++ src/main/java/io/github/nsdigirolamo/powerportals/utils/PortalActivationUtil.java
@@ -2,14 +2,17 @@ package io.github.nsdigirolamo.powerportals.utils;
 
 import io.github.nsdigirolamo.powerportals.PowerPortals;
 import io.github.nsdigirolamo.powerportals.structures.PowerPortal;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.type.Switch;
 import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * PortalActivationUtil is a utility that handles the activation of portals.
@@ -22,6 +25,12 @@ public class PortalActivationUtil {
      * @param portal The portal to be activated.
      */
     public static void activatePortal (Player player, PowerPortal portal) {
+        for (Block block: portal.getClearBlocks()) {
+            if (!block.getType().equals(Material.AIR)) {
+                player.sendMessage(ChatColor.RED + "[PowerPortals] " + portal.getName() + " failed to activate. Entrance is blocked.");
+                return;
+            }
+        }
         portal.setEntrance(true);
         player.setMetadata("activatedPortal", new FixedMetadataValue(PowerPortals.getPlugin(), portal));
 
@@ -34,7 +43,7 @@ public class PortalActivationUtil {
      * @param player The player who deactivated the portal.
      * @param portal The portal to be deactivated.
      */
-    public static void deactivatePortal (Player player, PowerPortal portal) {
+    public static void deactivatePortal(Player player, PowerPortal portal) {
 
         for (Block block: portal.getTriggerBlocks()) {
             block.setType(Material.AIR);
@@ -53,7 +62,20 @@ public class PortalActivationUtil {
         Switch lever = (Switch) portal.getLever().getBlockData();
         lever.setPowered(false);
         portal.getLever().setBlockData(lever);
+    }
 
+    /**
+     * Deactivates all PowerPortals.
+     */
+    public static void deactivateAllPortals () {
+        Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+        for (Player player: players) {
+            List<MetadataValue> values = player.getMetadata("activatedPortal");
+            if (!values.isEmpty() && values.get(0).value() instanceof PowerPortal) {
+                PowerPortal portal = (PowerPortal) values.get(0).value();
+                PortalActivationUtil.deactivatePortal(player, portal);
+            }
+        }
     }
 
 }
