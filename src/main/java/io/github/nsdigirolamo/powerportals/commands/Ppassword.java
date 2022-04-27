baae14b5d0b03592aa3c873c186bdd1b7331cf88
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2022-04-27 15:07:41 -0400

    ppassword command first commit

---
+++ src/main/java/io/github/nsdigirolamo/powerportals/commands/Ppassword.java
@@ -0,0 +1,73 @@
+package io.github.nsdigirolamo.powerportals.commands;
+
+import io.github.nsdigirolamo.powerportals.structures.PowerPortal;
+import io.github.nsdigirolamo.powerportals.utilities.StorageUtility;
+import io.github.nsdigirolamo.powerportals.utilities.interfaces.Messages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Ppassword implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+
+            Player player = (Player) sender;
+
+            if (player.hasPermission("powerportals.commands.ppassword")) {
+
+                if (args.length < 2) {
+                    player.sendMessage(Messages.TOO_FEW_ARGS);
+                } else if (args.length > 3) {
+                    player.sendMessage(Messages.TOO_MANY_ARGS);
+                } else {
+
+                    String portalName = args[0];
+                    PowerPortal portal = StorageUtility.findPortal(portalName);
+
+                    if (portal != null) {
+
+                        if (!portal.getOwnerID().equals(player.getUniqueId())) {
+                            player.sendMessage(Messages.RED_PREFIX + "Command failed. That portal does not belong to you.");
+                        } else {
+
+                            String action = args[1];
+
+                            if (action.equals("delete")) {
+
+                                portal.deletePassword();
+                                player.sendMessage(Messages.GREEN_PREFIX + "Password deleted.");
+
+                            } else if (action.equals("set")) {
+
+                                String password = args[2];
+
+                                if (password.matches("^[\\w]{1,20}$")) {
+
+                                    portal.setPassword(args[2]);
+                                    player.sendMessage(Messages.GREEN_PREFIX + "Password set to \"" + password + "\"");
+
+                                } else {
+
+                                    player.sendMessage(Messages.RED_PREFIX + "Command failed. Password must be " +
+                                            "between 1 - 20 characters and can only be letters and numbers");
+
+                                }
+                            } else {
+                                player.sendMessage(Messages.INVALID_ARGS);
+                            }
+                        }
+                    } else {
+                        player.sendMessage(Messages.PORTAL_DNE);
+                    }
+                }
+            } else {
+                player.sendMessage(Messages.NO_PERMISSION);
+            }
+        }
+        return true;
+    }
+}
